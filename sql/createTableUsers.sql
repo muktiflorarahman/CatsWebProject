64949a5ad019c07576b9f88c9906dfd26246c18c
Mukti Flora Rahman <mura1600@student.miun.se>
2022-11-12 09:12:59 +0100

    changed sql files

--- sql/createTableUsers.sql
+++ sql/createTableUsers.sql
@@ -6,7 +6,7 @@ CREATE TABLE users (
 );
 
 CREATE TABLE cats (
-    cats_id INT(11), 
+    cats_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     cats_breed TINYTEXT NOT NULL, 
     cats_info LONGTEXT NOT NULL,
     cats_picture TINYTEXT NOT NULL
---
+++ sql/insertIntoCats.sql
@@ -0,0 +1,9 @@
+INSERT INTO cats (cats_breed, cats_info, cats_picture)
+VALUES ('Bengal', 'Bengal honkatt', 'bengal.jpg'), 
+('Mainecoon', 'Mainecoon hankatt', 'mainecoon.jpeg'),
+('Manx', 'Manx honkatt', 'manx.jpg'),
+('Ragdoll', 'Ragdoll hankatt', 'ragdoll.jpeg'),
+('Siamese', 'Siamese honkatt', 'siamese.jpg'),
+('Sphynx', 'Sphynx honkatt', 'bengal.webp');
+
+
